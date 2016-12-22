08ce1c3acab5f98b638ae7517966a5229632e58f
MerrowBB <alex_xet@mail.ru>
2016-12-22 21:35:17 +0000

    The Shape class is operational

--- src/Shape.rb
+++ src/Shape.rb
@@ -1,19 +1,22 @@
 class Shape
-
-  include Math
-
-  properties :points
-
-  def initialize(*points)
+  def initialize(points)
     @points = points
   end
 
-  def rotate(*angles)
-
-    points.map &:rotate(*angles)
+  def points
+    @points
   end
 
   def center
-    
+    cen_x = (@points.max_by{ |pt| pt.x }.x + @points.min_by{ |pt| pt.x }.x) / 2
+    cen_y = (@points.max_by{ |pt| pt.y }.y + @points.min_by{ |pt| pt.y }.y) / 2
+    cen_z = (@points.max_by{ |pt| pt.z }.z + @points.min_by{ |pt| pt.z }.z) / 2
+    DPt[cen_x, cen_y, cen_z]
+  end
+
+  def rotate(*angles)
+    move_points = Array.new(@points.map { |pt| pt - center })
+    move_points = move_points.map { |pt| pt.spheric.rotate(*angles).descart }
+    Shape.new(move_points.map { |pt| pt + center })
   end
 end
